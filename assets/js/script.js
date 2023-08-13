89288a8201fee5d25ba46a0978a5b9d3ba9040d3
Deian13 <gorancaster@gmail.com>
2023-08-13 06:41:17 +0000

    Add rest of JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function showWindow(index) {
+    const floatingWindows = document.querySelectorAll('.floating-window');
+    const nextBtns = document.querySelectorAll('#nextBtn');
+    const resultDiv = document.getElementById("result");
+    let currentWindowIndex = 0;
 
+    function showWindow(index) {
+        floatingWindows[index].style.display = "block";
     }
 
     function hideWindow(index) {
+        floatingWindows[index].style.display = "none";
+    }
 
-}
+    function showNextWindow() {
+        hideWindow(currentWindowIndex);
+        currentWindowIndex++;
 
-function showNextWindow() {
+        if (currentWindowIndex < floatingWindows.length) {
+            showWindow(currentWindowIndex);
+        } else {
+            resultDiv.classList.remove("hidden");
+        }
+    }
 
-
+    showWindow(currentWindowIndex);
 
     nextBtns.forEach((btn) => {
+        btn.addEventListener("click", showNextWindow);
+    });
+});
