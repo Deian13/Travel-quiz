f6def5a833f20c76a5205f8999ccf1bd8c802add
Deian13 <gorancaster@gmail.com>
2023-08-13 06:22:57 +0000

    Added a js structure

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function showWindow(index) {
+
+    }
+
+    function hideWindow(index) {
+
+}
+
+function showNextWindow() {
+
+
+
+    nextBtns.forEach((btn) => {
